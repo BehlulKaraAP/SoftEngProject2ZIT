349dd630b48945a65316f99b97a17b205d3b6db4
behlul kara <behlulkara2005@gmail.com>
2026-09-01 16:41:45 +0200

    Change level 1

--- SoftProject/TileSets/SoftProjLevelTileSet.tsx
+++ SoftProject/TileSets/SoftProjLevelTileSet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.2" name="SoftProjLevelTileSet" tilewidth="16" tileheight="16" tilecount="7056" columns="84">
- <image source="../../../../../../Downloads/PixelPlatformerSet2v.1.1/main_lev_buildA.png" width="1344" height="1344"/>
+ <image source="main_lev_buildA.png" width="1344" height="1344"/>
 </tileset>
