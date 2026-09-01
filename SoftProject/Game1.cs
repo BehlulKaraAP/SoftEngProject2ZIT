8c1f702c5f6a46dc493ab9a070234f1d8d828ab1
behlul kara <behlulkara2005@gmail.com>
2026-09-01 22:44:14 +0200

    Add Game Over screen

--- SoftProject/Game1.cs
+++ SoftProject/Game1.cs
@@ -18,6 +18,7 @@ namespace SoftProject
         private List<Enemy> enemies;
 
         private LevelManager levelManager;
+        private Texture2D gameOverTexture;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -40,6 +41,7 @@ namespace SoftProject
             InitializeGameObject();
             // TODO: use this.Content to load your game content here
             heartTexture = Content.Load<Texture2D>("heart");
+            gameOverTexture = Content.Load<Texture2D>("GameOverScreen");
 
             enemyFactory = new EnemyFactory(Content, GraphicsDevice);
             enemies = new List<Enemy>();
@@ -62,6 +64,21 @@ namespace SoftProject
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            if (player.Health <= 0)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.R))
+                {
+                    // Reset player health
+                    player.Health = player.MaxHealth;
+
+                    // Reload Level 1 and reset enemies via the LevelManager
+                    levelManager.LoadLevel(1, player);
+                    player.Level = levelManager.currentLevel;
+                }
+
+                return; // FREEZES THE GAME: Skips updating player, enemies, and portals!
+            }
 
             // TODO: Add your update logic here
             if (Keyboard.GetState().IsKeyDown(Keys.T))
@@ -93,6 +110,11 @@ namespace SoftProject
             for (int i = 0; i < player.Health; i++)
             {
                 _spriteBatch.Draw(heartTexture, new Vector2(20 + (i * 40), 20), Color.White);
+            }
+
+            if (player.Health <= 0)
+            {
+                _spriteBatch.Draw(gameOverTexture, new Vector2(0, 0), Color.White);
             }
             _spriteBatch.End();
 
